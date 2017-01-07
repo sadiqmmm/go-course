ae8273f6b06427a47e561a41849f78058bf47b1e
Mohammed Sadiq <sadiqmmm@gmail.com>
2017-01-08 02:36:21 +0530

    Add the example for the datatype of Golang

--- src/vars.go
+++ src/vars.go
@@ -3,9 +3,27 @@ package main
 import "fmt"
 
 func main() {
+	
+	// Integer example
 	var a int32
 
 	a = 15
 
-	fmt.Println("Value of a:", a)	
+	fmt.Println("Value of integer a: ", a)	
+
+  // Float example
+	var f float32
+
+	f = 15.2
+
+	fmt.Println("Value of float f: ", f)
+
+	// boolean example
+
+	var set bool
+
+	set = true
+
+	fmt.Println("Value of bool set: ", set)
+
 }
